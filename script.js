b1a4983acf0af1f36d81bd70e69f3da1e42047c8
faizarsalan <faizarsalan@github.com>
2021-06-13 00:36:50 +0700

    made slider.js and script.js
    slider.js for the image slider
    script.js for the form registration

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// Image Slider
-$(document).ready(function(){
-    $('.next').on('click', function(){
-      var currentImg = $('.active');
-      var nextImg = currentImg.next();
-  
-      if(nextImg.length){
-        currentImg.removeClass('active').css('z-index', -10);
-        nextImg.addClass('active').css('z-index', 10);
-      }
-    });
-  
-    $('.prev').on('click', function(){
-      var currentImg = $('.active');
-      var prevImg = currentImg.prev();
-  
-      if(prevImg.length){
-        currentImg.removeClass('active').css('z-index', -10);
-        prevImg.addClass('active').css('z-index', 10);
-      }
-    });
-  });
-
 // Registration Form
 const form = document.getElementById('form');
 const username = document.getElementById('username');
